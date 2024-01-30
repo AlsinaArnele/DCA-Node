9d3ddd157aeaf78399d868a365420abb742bd4e1
Alsina Arnele <[leviskibet2002@gmail.com]>
2024-01-30 13:05:59 +0300

    ...

--- index.js
+++ index.js
@@ -20,18 +20,18 @@ app.use(express.static('public/favicon'));
 // Set up session
 app.use(session({
     secret: 'lsinaaarnele',
-    resave: false,
+    resave: true,
     saveUninitialized: true,
 }));
 
 // enforce session
 const requireAuth = (req, res, next) => {
     if (!req.session.username) {
-        res.redirect('/', { errorMessage: 'Please login to view this page' });
+        res.redirect(302, '/?errorMessage=Please%20login%20to%20view%20this%20page');
     }
     next();
 };
-app.use(['/dashboard', '/profile'], requireAuth);
+// app.use(['/dashboard', '/profile'], requireAuth);
 
 // Global variable to store the database connection
 const connection = mysql.createConnection({
@@ -45,6 +45,38 @@ const connection = mysql.createConnection({
 app.get('/', (req, res) => {
     res.render('login', { errorMessage: '' });
 });
+
+// registration page route
+app.get('/register', (req, res) => {
+    res.render('registration', { errorMessage: '' });
+});
+
+// dashboard logic
+const getServices = () => {
+    const query = 'SELECT * FROM services';
+    return new Promise((resolve, reject) => {
+        connection.query(query, (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+};
+
+
+// dashboard route
+app.get('/dashboard', async (req, res) => {
+    try {
+        const services = await getServices();
+        res.render('dashboard', { services });
+    } catch (error) {
+        console.error('Error fetching services:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 
 // login form handling
 app.post('/', (req, res) => {
@@ -63,9 +95,7 @@ app.post('/', (req, res) => {
                 if (user.status === 'active') {
                     if (isPasswordValid) {
                         app.get('/set-session', (req, res) => {
-                            // Set a session variable
                             req.session.username = email;
-                            res.send('Session variable set');
                         });
                         res.redirect('/dashboard');
                     } else {
@@ -80,12 +110,6 @@ app.post('/', (req, res) => {
         });
     };
     authenticateUser(email, password);
-});
-
-
-// registration page route
-app.get('/register', (req, res) => {
-    res.render('registration', { errorMessage: '' });
 });
 
 // registration form handling
@@ -125,7 +149,14 @@ app.post('/register', (req, res) => {
                         return;
                     }
                     console.log('User inserted successfully');
-        
+                    const insertServices = 'INSERT INTO services (user_email, slack, DCA_drive, leads, hosting) VALUES (?, ?, ?, ?, ?)';
+                    connection.query(insertServices, [email, '0', '0', '0', '0', '0', '0'], (err, results) => {
+                        if (err) {
+                            console.error('Error inserting services:', err);
+                            return;
+                        }
+                        console.log('Services inserted successfully');
+                    });
                     // Generate a verification token and email it
                     const verificationToken = (Math.floor(100000 + Math.random() * 900000));
                     // insert code into db
@@ -147,22 +178,8 @@ app.post('/register', (req, res) => {
     });
 });
 
-// dashboard route
-app.get('/dashboard', (req, res) => {
-    res.render('dashboard');
-});
-
-// dashboard logic
-
-
-// logout route
-app.get('/logout', (req, res) => {
-    res.redirect('/');
-});
-
 // Emailing
 const nodemailer = require('nodemailer');
-
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -176,7 +193,7 @@ const sendVerificationEmail = (to, verificationToken) => {
         from: 'leviskibet2002@gmail.com',
         to,
         subject: 'Email Verification',
-        // html: `<p>Click the following link to verify your email: <a href="http://portal.dotconnectafrica.org/login/${verificationToken}">Verify Email</a></p>`
+        html: `<p>Click the following link to verify your email: <a href="http://portal.dotconnectafrica.org/login/${verificationToken}">Verify Email</a></p>`,
         // for testing environment
         html: `<p>Click the following link to verify your email: <a href="http://localhost:3000//${verificationToken}">Verify Email</a></p>`
     };
@@ -191,7 +208,7 @@ const sendVerificationEmail = (to, verificationToken) => {
 };
 
 
-// route for handling email verification
+// email verification
 app.get('//:token', (req, res) => {
     const verificationToken = req.params.token;
     const query = 'SELECT * FROM tokens WHERE token_value = ?';
